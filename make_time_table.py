c66f21f60713bd52ba0431c16be5a12eed589413
IHA Yasushi <yasuc18@gmail.com>
2021-05-27 17:06:27 +0900

    引数により開始回数、終了回数、前期後期などを指定できるようにした

--- make_time_table.py
+++ make_time_table.py
@@ -3,18 +3,20 @@ import sys
 import pprint
 import pickle
 import os
-from pathlib import Path
 import datetime
+import argparse
+from pathlib import Path
 
 time_range = [
 	["9:00", "10:30"],
 	["10:40", "12:10"],
-	["13:30", "15:00"],
-	["15:10", "16:40"],
-	["13:30", "16:40"],
+	["14:00", "15:30"],
+	["15:40", "17:10"],
+	["14:00", "17:10"],
 ]
 
 subjects = [
+        [
 #    [ 曜日,
 #      科目名
 #      時限(1-4 and 5(3+4)),
@@ -28,14 +30,17 @@ subjects = [
     [3, "卒業研究", 5, "OLab", 0],
     [4, "情報セキュリティI", 2, "NW演習室", 0],
     [4, "卒業研究", 5, "OLab", 0],
-# 未定
-#    [4, "科学技術英語", 2, "OLab", 0],
+    [5, "科学技術英語", 2, "OLab", 0],
+    ],[
 
 # 後期用
-#    [1, "情報セキュリティII", "10:40", "12:10", "講義室1-1:2", 0],
-#    [1, "情報セキュリティII", "14:00", "15:30", "講義室1-1:2", 0],
-#    [2, "創造研究", "14:40", "16:10", "NW演習室"],
-#    [4, "OSとコンパイラI", "10:40", "12:10", "講義室1-1:2", 0],
+    [1, "情報セキュリティII", 2, "講義室1-1:2", 0],
+    [1, "情報セキュリティII", 3, "講義室1-1:2", 0],
+    [3, "卒業研究", 5, "OLab", 0],
+    [3, "コンピュータネットワークI", 2, "講2-2", 0],
+    [4, "OSとコンパイラI", 2, "講義室1-1:2", 0],
+    [4, "卒業研究", 5, "OLab", 0],
+]
 ]
 
 def get_value_list(t_2d):
@@ -65,23 +70,32 @@ def old_pklfile_del(xlsx_file, pkl_file):
     if xlsx_update_time > pkl_update_time:
         os.remove(pklfile)
 
-
-#前期
-c = 1
-mon = 5
-#後期
-#c = 86
-#mon = 5
-
 all_2d = []
 
-xlsx_name = 'schedule.xlsx'
-pklfile = "make_time_schedule.pkl"
+parser = argparse.ArgumentParser(description="時間割作成プログラム")
 
-args = sys.argv
+parser.add_argument('-x', '--xlsx', default='schedule.xlsx', help='スケジュールExcelファイル')
+parser.add_argument('-s', '--start', default=1, type=int, help='開始回')
+parser.add_argument('-e', '--end', default=15, type=int, help='修了回')
+parser.add_argument('-t', '--term', default=1, type=int, help='前後期(1:前期, 2:後期)')
 
-if len(args) == 2:
-    xlsx_name = args[1]
+args = parser.parse_args()
+
+xlsx_name = args.xlsx
+start = args.start
+end = args.end
+term = args.term
+
+if term == 1:
+    #前期
+    c = 1
+    mon = 5
+elif term ==2:
+    #後期
+    c = 86
+    mon = 6
+
+pklfile = "make_time_schedule_%i.pkl" % term
 
 old_pklfile_del(xlsx_name, pklfile)
 
@@ -101,22 +115,15 @@ else:
         all_2d = pickle.load(f)
 
 print("Subject,Start Date,Start Time,End Date,End Time")
-sotuken = False
-for s in subjects:
+for s in subjects[term - 1]:
     n = 1
-
-#    if s[1] != "卒業研究":
-#        n = 1
-#    if s[1] == "卒業研究" and not sotuken:
-#        n = 1
-#        sotuken = True
-
     for l_2d in all_2d:
         for t in l_2d:
             if t[s[0] + 6 + s[-1]*5] is not None:
-                day = excel_date(t[0]).strftime('%Y/%m/%d')
-                csv = "講義:{:s}[{:s}]:{:d},{:s},{:s},{:s},{:s}".format(
-                    s[1], s[3], n, day, time_range[s[2]-1][0], day, time_range[s[2]-1][1])
-                print(csv)
+                if n >= start and n <= end:
+                    day = excel_date(t[0]).strftime('%Y/%m/%d')
+                    csv = "講義:{:s}[{:s}]:{:d},{:s},{:s},{:s},{:s}".format(
+                        s[1], s[3], n, day, time_range[s[2]-1][0], day, time_range[s[2]-1][1])
+                    print(csv)
                 n += 1
 
