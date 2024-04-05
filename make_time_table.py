27f6086067d0e540531706ed033c7a404a8b87f4
IHA Yasushi <yasuc18@gmail.com>
2024-04-05 16:25:46 +0900

    make_time_table.pyのリファクタリング

--- make_time_table.py
+++ make_time_table.py
@@ -61,7 +61,6 @@ def excel_date(num):
     from datetime import datetime, timedelta
 
     return num
-    #return datetime(1899, 12, 30) + timedelta(days=num)
 
 
 def old_pklfile_del(xlsx_file, pkl_file):
@@ -78,66 +77,75 @@ def old_pklfile_del(xlsx_file, pkl_file):
         os.remove(pklfile)
 
 
-all_2d = []
+def main():
+    all_2d = []
 
-parser = argparse.ArgumentParser(description="時間割作成プログラム")
+    parser = argparse.ArgumentParser(description="時間割作成プログラム")
 
-parser.add_argument("-x", "--xlsx", default="schedule.xlsx", help="スケジュールExcelファイル")
-parser.add_argument("-s", "--start", default=1, type=int, help="開始回")
-parser.add_argument("-e", "--end", default=15, type=int, help="終了回")
-parser.add_argument("-t", "--term", default=1, type=int, help="前後期(1:前期, 2:後期)")
+    parser.add_argument(
+        "-x", "--xlsx", default="schedule.xlsx", help="スケジュールExcelファイル"
+    )
+    parser.add_argument("-s", "--start", default=1, type=int, help="開始回")
+    parser.add_argument("-e", "--end", default=15, type=int, help="終了回")
+    parser.add_argument(
+        "-t", "--term", default=1, type=int, help="前後期(1:前期, 2:後期)"
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-xlsx_name = args.xlsx
-start = args.start
-end = args.end
-term = args.term
+    xlsx_name = args.xlsx
+    start = args.start
+    end = args.end
+    term = args.term
 
-if term == 1:
-    # 前期
-    c = 1
-    mon = 5
-elif term == 2:
-    # 後期
-    c = 86
-    mon = 6
+    if term == 1:
+        # 前期
+        c = 1
+        mon = 5
+    elif term == 2:
+        # 後期
+        c = 86
+        mon = 6
 
-pklfile = "make_time_schedule_%i.pkl" % term
+    pklfile = "make_time_schedule_%i.pkl" % term
 
-old_pklfile_del(xlsx_name, pklfile)
+    old_pklfile_del(xlsx_name, pklfile)
 
-if not os.path.isfile(pklfile):
-    print("Making pkl file.", file=sys.stderr)
-    wb = px.load_workbook(xlsx_name, data_only=True)
+    if not os.path.isfile(pklfile):
+        print("Making pkl file.", file=sys.stderr)
+        wb = px.load_workbook(xlsx_name, data_only=True)
 
-    sheet = wb.active
+        sheet = wb.active
 
-    for i in range(mon):
-        all_2d.append(get_list_2d(sheet, 5, 128, c, c + 16))
-        c += 17
-    with open(pklfile, "wb") as f:
-        pickle.dump(all_2d, f)
-else:
-    with open(pklfile, "rb") as f:
-        all_2d = pickle.load(f)
+        for i in range(mon):
+            all_2d.append(get_list_2d(sheet, 5, 128, c, c + 16))
+            c += 17
+        with open(pklfile, "wb") as f:
+            pickle.dump(all_2d, f)
+    else:
+        with open(pklfile, "rb") as f:
+            all_2d = pickle.load(f)
 
-print("Subject,Start Date,Start Time,End Date,End Time")
-for s in subjects[term - 1]:
-    n = 1
-    for l_2d in all_2d:
-        for t in l_2d:
-            if t[s[0] + 6 + s[-1] * 5] is not None:
-                if n >= start and n <= end:
-                    day = excel_date(t[0]).strftime("%Y/%m/%d")
-                    csv = "講義:{:s}[{:s}]:{:d},{:s},{:s},{:s},{:s}".format(
-                        s[1],
-                        s[3],
-                        n,
-                        day,
-                        time_range[s[2] - 1][0],
-                        day,
-                        time_range[s[2] - 1][1],
-                    )
-                    print(csv)
-                n += 1
+    print("Subject,Start Date,Start Time,End Date,End Time")
+    for s in subjects[term - 1]:
+        n = 1
+        for l_2d in all_2d:
+            for t in l_2d:
+                if t[s[0] + 6 + s[-1] * 5] is not None:
+                    if n >= start and n <= end:
+                        day = excel_date(t[0]).strftime("%Y/%m/%d")
+                        csv = "講義:{:s}[{:s}]:{:d},{:s},{:s},{:s},{:s}".format(
+                            s[1],
+                            s[3],
+                            n,
+                            day,
+                            time_range[s[2] - 1][0],
+                            day,
+                            time_range[s[2] - 1][1],
+                        )
+                        print(csv)
+                    n += 1
+
+
+if __name__ == "__main__":
+    main()
